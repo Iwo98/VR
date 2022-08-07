d6e38e641dbd8dee35e35342fc36bab2b9d95185
Iwo <iwo.brzyski98@gmail.com>
2022-08-07 13:57:00 +0200

    fixed two games

--- MemoryGamesVR/Assets/GlobalScripts/VRHandInputManager.cs
+++ MemoryGamesVR/Assets/GlobalScripts/VRHandInputManager.cs
@@ -10,33 +10,95 @@ public class VRHandInputManager : MonoBehaviour
     public bool rightTriggerPressed = false;
     public bool leftTriggerPressed = false;
     // Start is called before the first frame update
+    /* void Start()
+     {
+         List<InputDevice> devices = new List<InputDevice>();
+         InputDevices.GetDevices(devices);
+         Debug.Log(" === Detected devices: ===");
+         foreach (var device in devices)
+         {
+             Debug.Log(device.name + device.characteristics);
+             if(device.name.Contains("Right")) {
+                 rightController = device;
+                 rightTriggerPressed = false;
+             }
+             else if (device.name.Contains("Left"))
+             {
+                 leftController = device;
+                 leftTriggerPressed = false;
+             }
+         }
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+         rightController.TryGetFeatureValue(CommonUsages.triggerButton, out bool rTriggerValue);
+         rightTriggerPressed = rTriggerValue;
+         leftController.TryGetFeatureValue(CommonUsages.triggerButton, out bool lTriggerValue);
+         leftTriggerPressed = lTriggerValue;
+     }*/
+    //private InputDevice _targetDevice;
+    // Start is called before the first frame update
     void Start()
     {
-        List<InputDevice> devices = new List<InputDevice>();
-        InputDevices.GetDevices(devices);
+        TryInitializeRight();
+        TryInitializeLeft();
+    }
 
-        //Debug.Log(" === Detected devices: ===");
-        foreach (var device in devices)
+    void TryInitializeRight()
+    {
+        var inputDevices = new List<InputDevice>();
+        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, inputDevices);
+
+        if (inputDevices.Count == 0)
         {
-            //Debug.Log(device.name + device.characteristics);
-            if(device.name.Contains("Right")) {
-                rightController = device;
-                rightTriggerPressed = false;
-            }
-            else if (device.name.Contains("Left"))
-            {
-                leftController = device;
-                leftTriggerPressed = false;
-            }
+            return;
+        }
+        else
+        {
+            rightController = inputDevices[0];
+        }
+    }
+
+    void TryInitializeLeft()
+    {
+        var inputDevices = new List<InputDevice>();
+        InputDeviceCharacteristics leftControllerCharacteristics = InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(leftControllerCharacteristics, inputDevices);
+
+        if (inputDevices.Count == 0)
+        {
+            return;
+        }
+        else
+        {
+            leftController = inputDevices[0];
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        rightController.TryGetFeatureValue(CommonUsages.triggerButton, out bool rTriggerValue);
-        rightTriggerPressed = rTriggerValue;
-        leftController.TryGetFeatureValue(CommonUsages.triggerButton, out bool lTriggerValue);
-        leftTriggerPressed = lTriggerValue;
+        if (!rightController.isValid)
+        {
+            TryInitializeRight();
+        }
+        else
+        {
+            rightController.TryGetFeatureValue(CommonUsages.triggerButton, out bool rTriggerValue);
+            rightTriggerPressed = rTriggerValue;
+        }
+
+        if (!leftController.isValid)
+        {
+            TryInitializeLeft();
+        }
+        else
+        {
+            leftController.TryGetFeatureValue(CommonUsages.triggerButton, out bool lTriggerValue);
+            leftTriggerPressed = lTriggerValue;
+        }
     }
 }
